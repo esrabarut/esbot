859f028d4b9cd0246761b92a16dd2a69e7f62d8f
ESRA BARUT <esjack07@gmail.com>
2022-12-03 01:31:10 +0300

    Hello discord guys!

---
+++ settings/ayarlar.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+module.exports = {
+    //TOKEN: process.env.TOKEN || "TOKEN GİR",  // bot token gir :)
+    PREFIX: process.env.PREFIX || "!", // bot prefix
+    SAHİP: process.env.SAHİP || "303825366756229130", //sahip id :()
+}
+
